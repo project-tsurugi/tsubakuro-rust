3c130ba7d6a57168967fa05a1365549e529a7c32
Hishida Masato <hishida@nautilus-technologies.com>
2024-12-23 18:22:35 +0900

    feat: SqlPlaceholder::of_type()

--- tsubakuro-rust-core/src/service/sql/prepare/placeholder.rs
+++ tsubakuro-rust-core/src/service/sql/prepare/placeholder.rs
@@ -39,6 +39,52 @@ impl SqlPlaceholderOf<AtomType> for SqlPlaceholder {
     }
 }
 
+impl SqlPlaceholder {
+    pub fn of_type<T: AtomTypeProvider>(name: &str) -> Self {
+        let atom_type = T::atom_type();
+        SqlPlaceholder::of(name, atom_type)
+    }
+}
+
+pub trait AtomTypeProvider {
+    fn atom_type() -> AtomType;
+}
+
+impl AtomTypeProvider for i32 {
+    fn atom_type() -> AtomType {
+        AtomType::Int4
+    }
+}
+
+impl AtomTypeProvider for i64 {
+    fn atom_type() -> AtomType {
+        AtomType::Int8
+    }
+}
+
+impl AtomTypeProvider for f32 {
+    fn atom_type() -> AtomType {
+        AtomType::Float4
+    }
+}
+
+impl AtomTypeProvider for f64 {
+    fn atom_type() -> AtomType {
+        AtomType::Float8
+    }
+}
+impl AtomTypeProvider for &str {
+    fn atom_type() -> AtomType {
+        AtomType::Character
+    }
+}
+
+impl AtomTypeProvider for String {
+    fn atom_type() -> AtomType {
+        AtomType::Character
+    }
+}
+
 pub trait SqlPlaceholderGenerator {
     fn placeholder(name: &str) -> SqlPlaceholder;
 }
@@ -79,6 +125,31 @@ impl SqlPlaceholderGenerator for String {
     }
 }
 
+impl SqlPlaceholder {
+    pub fn placeholder_for<T: AtomTypeProvider>(name: &str) -> Self {
+        let atom_type = T::atom_type();
+        SqlPlaceholder::of(name, atom_type)
+    }
+}
+
+pub trait SqlPlaceholderFrom {
+    fn placeholder_from<A: AtomTypeProvider>(self) -> SqlPlaceholder;
+}
+
+impl SqlPlaceholderFrom for &str {
+    fn placeholder_from<A: AtomTypeProvider>(self) -> SqlPlaceholder {
+        let atom_type = A::atom_type();
+        SqlPlaceholder::of(self, atom_type)
+    }
+}
+
+impl SqlPlaceholderFrom for String {
+    fn placeholder_from<A: AtomTypeProvider>(self) -> SqlPlaceholder {
+        let atom_type = A::atom_type();
+        SqlPlaceholder::of(&self, atom_type)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -89,7 +160,13 @@ mod test {
         assert_eq!("test", target0.name().unwrap());
         assert_eq!(AtomType::Int4, target0.atom_type().unwrap());
 
+        let target = SqlPlaceholder::of_type::<i32>("test");
+        assert_eq!(target0, target);
+
         let target = i32::placeholder("test");
+        assert_eq!(target0, target);
+
+        let target = "test".placeholder_from::<i32>();
         assert_eq!(target0, target);
     }
 
@@ -99,7 +176,13 @@ mod test {
         assert_eq!("test", target0.name().unwrap());
         assert_eq!(AtomType::Int8, target0.atom_type().unwrap());
 
+        let target = SqlPlaceholder::of_type::<i64>("test");
+        assert_eq!(target0, target);
+
         let target = i64::placeholder("test");
+        assert_eq!(target0, target);
+
+        let target = "test".placeholder_from::<i64>();
         assert_eq!(target0, target);
     }
 
@@ -109,7 +192,13 @@ mod test {
         assert_eq!("test", target0.name().unwrap());
         assert_eq!(AtomType::Float4, target0.atom_type().unwrap());
 
+        let target = SqlPlaceholder::of_type::<f32>("test");
+        assert_eq!(target0, target);
+
         let target = f32::placeholder("test");
+        assert_eq!(target0, target);
+
+        let target = "test".placeholder_from::<f32>();
         assert_eq!(target0, target);
     }
 
@@ -119,7 +208,13 @@ mod test {
         assert_eq!("test", target0.name().unwrap());
         assert_eq!(AtomType::Float8, target0.atom_type().unwrap());
 
+        let target = SqlPlaceholder::of_type::<f64>("test");
+        assert_eq!(target0, target);
+
         let target = f64::placeholder("test");
+        assert_eq!(target0, target);
+
+        let target = "test".placeholder_from::<f64>();
         assert_eq!(target0, target);
     }
 
@@ -129,7 +224,13 @@ mod test {
         assert_eq!("test", target0.name().unwrap());
         assert_eq!(AtomType::Character, target0.atom_type().unwrap());
 
+        let target = SqlPlaceholder::of_type::<&str>("test");
+        assert_eq!(target0, target);
+
         let target = str::placeholder("test");
+        assert_eq!(target0, target);
+
+        let target = "test".placeholder_from::<&str>();
         assert_eq!(target0, target);
     }
 
@@ -139,7 +240,13 @@ mod test {
         assert_eq!("test", target0.name().unwrap());
         assert_eq!(AtomType::Character, target0.atom_type().unwrap());
 
+        let target = SqlPlaceholder::of_type::<String>("test");
+        assert_eq!(target0, target);
+
         let target = String::placeholder("test");
+        assert_eq!(target0, target);
+
+        let target = "test".placeholder_from::<String>();
         assert_eq!(target0, target);
     }
 }
