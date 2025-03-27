d437f2e7d1a37f4ad8fc944bb6a60ac08c9df03b
Hishida Masato <hishida@nautilus-technologies.com>
2025-03-27 12:01:24 +0900

    docs: Update example

--- tsubakuro-rust-dbtest/src/bin/example2.rs
+++ tsubakuro-rust-dbtest/src/bin/example2.rs
@@ -3,11 +3,25 @@ use std::time::Duration;
 use log::warn;
 use tsubakuro_rust_core::prelude::*;
 
+/*
+The table should be created before execution.
+
+create table customer (
+    c_id   bigint primary key,
+    c_name varchar(30),
+    c_age  int
+);
+insert into customer values(1, 'Hello', 51);
+insert into customer values(2, 'World', 138);
+insert into customer values(3, 'Tsurugi', 1);
+*/
+
 #[tokio::main]
 async fn main() -> Result<(), TgError> {
     example().await
 }
 
+/// connect example
 async fn example() -> Result<(), TgError> {
     let endpoint = Endpoint::parse("tcp://localhost:12345")?;
 
@@ -34,6 +48,7 @@ async fn example() -> Result<(), TgError> {
     result
 }
 
+/// transaction example
 async fn example_transaction(client: &SqlClient) -> Result<(), TgError> {
     // transaction start
     let mut transaction_option = TransactionOption::new();
@@ -57,15 +72,20 @@ async fn example_transaction(client: &SqlClient) -> Result<(), TgError> {
     result
 }
 
+/// execute SQLs
 async fn example_sql(client: &SqlClient, transaction: &Transaction) -> Result<(), TgError> {
     example_statement(client, transaction).await?;
     example_query(client, transaction).await?;
     example_prepared_statement_insert(client, transaction).await?;
-    example_prepared_statement_query(client, transaction).await?;
+    example_prepared_statement_query0(client, transaction).await?;
+    example_prepared_statement_query1(client, transaction).await?;
     Ok(())
 }
 
+/// execute SQL (update) example
 async fn example_statement(client: &SqlClient, transaction: &Transaction) -> Result<(), TgError> {
+    println!("---example_statement---");
+
     let sql = "update customer set c_age = 2 where c_id = 3";
     let execute_result = client.execute(&transaction, sql).await?;
     println!("updated_rows={}", execute_result.updated_rows());
@@ -73,7 +93,10 @@ async fn example_statement(client: &SqlClient, transaction: &Transaction) -> Res
     Ok(())
 }
 
+/// execute SQL (select) example
 async fn example_query(client: &SqlClient, transaction: &Transaction) -> Result<(), TgError> {
+    println!("---example_query---");
+
     let sql = "select c_id, c_name, c_age from customer order by c_id";
     let mut query_result = client.query(&transaction, sql).await?;
 
@@ -93,10 +116,13 @@ async fn example_query(client: &SqlClient, transaction: &Transaction) -> Result<
     Ok(())
 }
 
+/// PreparedStatement (insert) example
 async fn example_prepared_statement_insert(
     client: &SqlClient,
     transaction: &Transaction,
 ) -> Result<(), TgError> {
+    println!("---example_prepared_statement_insert---");
+
     let sql = "insert into customer values(:id, :name, :age)";
     let placeholders = vec![
         SqlPlaceholder::of::<i64>("id"),
@@ -114,6 +140,7 @@ async fn example_prepared_statement_insert(
     result
 }
 
+/// execute PreparedStatement (insert) example
 async fn example_prepared_execute(
     client: &SqlClient,
     transaction: &Transaction,
@@ -130,15 +157,18 @@ async fn example_prepared_execute(
     Ok(())
 }
 
-async fn example_prepared_statement_query(
+/// PreparedStatement (select) example
+async fn example_prepared_statement_query0(
     client: &SqlClient,
     transaction: &Transaction,
 ) -> Result<(), TgError> {
+    println!("---example_prepared_statement_query0---");
+
     let sql = "select c_id, c_name, c_age from customer order by c_id";
     let placeholders = vec![];
     let ps = client.prepare(sql, placeholders).await?;
 
-    let result = example_prepared_query(client, transaction, &ps).await;
+    let result = example_prepared_query0(client, transaction, &ps).await;
 
     if let Err(e) = ps.close().await {
         warn!("preparedStatement close error. {}", e);
@@ -147,12 +177,58 @@ async fn example_prepared_statement_query(
     result
 }
 
-async fn example_prepared_query(
+/// execute PreparedStatement (select) example
+async fn example_prepared_query0(
     client: &SqlClient,
     transaction: &Transaction,
     ps: &SqlPreparedStatement,
 ) -> Result<(), TgError> {
     let parameters = vec![];
+    let mut query_result = client.prepared_query(transaction, ps, parameters).await?;
+
+    while query_result.next_row().await? {
+        assert!(query_result.next_column().await?);
+        let id: i64 = query_result.fetch().await?; // not null
+
+        assert!(query_result.next_column().await?);
+        let name: Option<String> = query_result.fetch().await?; // nullable
+
+        assert!(query_result.next_column().await?);
+        let age: Option<i32> = query_result.fetch().await?; // nullable
+
+        println!("id={id}, name={name:?}, age={age:?}");
+    }
+
+    Ok(())
+}
+
+/// PreparedStatement (select) example
+async fn example_prepared_statement_query1(
+    client: &SqlClient,
+    transaction: &Transaction,
+) -> Result<(), TgError> {
+    println!("---example_prepared_statement_query1---");
+
+    let sql = "select * from customer where c_id = :id";
+    let placeholders = vec![SqlPlaceholder::of::<i64>("id")];
+    let ps = client.prepare(sql, placeholders).await?;
+
+    let result = example_prepared_query1(client, transaction, &ps).await;
+
+    if let Err(e) = ps.close().await {
+        warn!("preparedStatement close error. {}", e);
+    }
+
+    result
+}
+
+/// execute PreparedStatement (select) example
+async fn example_prepared_query1(
+    client: &SqlClient,
+    transaction: &Transaction,
+    ps: &SqlPreparedStatement,
+) -> Result<(), TgError> {
+    let parameters = vec![SqlParameter::of("id", 3_i64)];
     let mut query_result = client.prepared_query(transaction, ps, parameters).await?;
 
     while query_result.next_row().await? {
