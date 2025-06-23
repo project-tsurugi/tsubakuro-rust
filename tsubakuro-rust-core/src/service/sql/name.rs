c00ee8f5ab89c4ccf320b24e978f5a586d3c5137
Hishida Masato <hishida@nautilus-technologies.com>
2025-06-24 07:28:03 +0900

    feat: TName::last_name()

--- tsubakuro-rust-core/src/service/sql/name.rs
+++ tsubakuro-rust-core/src/service/sql/name.rs
@@ -25,6 +25,35 @@ impl TName {
     pub fn identifiers(&self) -> &Vec<String> {
         &self.identifiers
     }
+
+    /// Get database name.
+    ///
+    /// since 0.3.0
+    pub fn database_name(&self) -> Option<&String> {
+        let index = self.identifiers.len() as isize - 1 - 2;
+        if index < 0 {
+            return None;
+        }
+        self.identifiers.get(index as usize)
+    }
+
+    /// Get schema name.
+    ///
+    /// since 0.3.0
+    pub fn schema_name(&self) -> Option<&String> {
+        let index = self.identifiers.len() as isize - 1 - 1;
+        if index < 0 {
+            return None;
+        }
+        self.identifiers.get(index as usize)
+    }
+
+    /// Get last name.
+    ///
+    /// since 0.3.0
+    pub fn last_name(&self) -> Option<&String> {
+        self.identifiers.last()
+    }
 }
 
 impl std::fmt::Display for TName {
@@ -47,31 +76,39 @@ mod test {
     #[test]
     fn tname_identifiers() {
         let name = TName::new(vec![
-            String::from("scheme1"),
             String::from("database1"),
+            String::from("schema1"),
             String::from("table1"),
         ]);
 
         let i = name.identifiers();
         assert_eq!(3, i.len());
-        assert_eq!(String::from("scheme1"), i[0]);
-        assert_eq!(String::from("database1"), i[1]);
+        assert_eq!(String::from("database1"), i[0]);
+        assert_eq!(String::from("schema1"), i[1]);
         assert_eq!(String::from("table1"), i[2]);
+
+        assert_eq!(Some(&String::from("database1")), name.database_name());
+        assert_eq!(Some(&String::from("schema1")), name.schema_name());
+        assert_eq!(Some(&String::from("table1")), name.last_name());
     }
 
     #[test]
     fn tname_to_string1() {
         let name = TName::new(vec![String::from("table1")]);
         assert_eq!("table1", name.to_string());
+
+        assert_eq!(None, name.database_name());
+        assert_eq!(None, name.schema_name());
+        assert_eq!(Some(&String::from("table1")), name.last_name());
     }
 
     #[test]
     fn tname_to_string3() {
         let name = TName::new(vec![
-            String::from("scheme1"),
             String::from("database1"),
+            String::from("schema1"),
             String::from("table1"),
         ]);
-        assert_eq!("scheme1.database1.table1", name.to_string());
+        assert_eq!("database1.schema1.table1", name.to_string());
     }
 }
