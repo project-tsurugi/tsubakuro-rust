6c8d65b2af8781f2741219dddaf3ca35a1cc5921
Hishida Masato <hishida@nautilus-technologies.com>
2025-07-12 14:09:39 +0900

    fix: env.TSURUGI_ODBC_DRIVER_LOGLEVEL

--- tsubakuro-rust-odbc/docs/usage_ja.md
+++ tsubakuro-rust-odbc/docs/usage_ja.md
@@ -64,9 +64,16 @@ Tsurugiの接続先として、接続文字列内にエンドポイントを記�
 
 tsubakuro-rust-odbcは [env_logger](https://crates.io/crates/env_logger) を使ってログ出力しています。
 
-Tsurugi ODBC Driverを呼び出すアプリケーションの実行時に 環境変数RUST_LOGを設定しておくと、標準エラーにトレースログやデバッグログが出力されます。
+Tsurugi ODBCドライバーを呼び出すアプリケーションの実行時に 環境変数TSURUGI_ODBC_DRIVER_LOGLEVELを設定しておくと、トレースログやデバッグログが出力されます。  
+（設定値は、env_loggerの環境変数RUST_LOGの仕様と同じです）
 
 ```dos
-set RUST_LOG=tsubakuro_rust_odbc=trace
+set TSURUGI_ODBC_DRIVER_LOGLEVEL=tsubakuro_rust_odbc=trace
+```
+
+ログの出力先は、デフォルトではコンソールの標準エラーですが、環境変数TSURUGI_ODBC_DRIVER_LOGFILEにログファイルのパスを指定することで、ファイル出力に切り替えることができます。
+
+```dos
+set TSURUGI_ODBC_DRIVER_LOGFILE=/path/to/logfile
 ```
 
--- tsubakuro-rust-odbc/src/logger.rs
+++ tsubakuro-rust-odbc/src/logger.rs
@@ -1,6 +1,9 @@
 use std::sync::Once;
 
+use env_logger::{Builder, Target};
 use log::trace;
+
+use crate::DRIVER_NAME;
 
 static ENV_LOGGER_INIT: Once = Once::new();
 
@@ -9,8 +12,51 @@ pub(crate) fn env_logger_init() {
         const FUNCTION_NAME: &str = "env_logger_init()";
         trace!("{FUNCTION_NAME} start");
 
-        env_logger::builder().format_timestamp_millis().init();
+        let _ = env_logger_init_main();
 
         trace!("{FUNCTION_NAME} end");
     })
+}
+
+fn env_logger_init_main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut builder = Builder::new();
+
+    if let Ok(filters) = std::env::var("TSURUGI_ODBC_DRIVER_LOGLEVEL") {
+        builder.parse_filters(&filters);
+    } else {
+        return Ok(());
+    }
+
+    if let Ok(path) = std::env::var("TSURUGI_ODBC_DRIVER_LOGFILE") {
+        env_logger_init_file(&mut builder, &path)?
+    }
+
+    builder.format_timestamp_millis();
+
+    if let Err(e) = builder.try_init() {
+        eprintln!("{DRIVER_NAME}: env_logger init fail. {}", e);
+        return Err(e.into());
+    }
+
+    Ok(())
+}
+
+fn env_logger_init_file(
+    builder: &mut Builder,
+    file_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let file = match std::fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(file_path)
+    {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("{DRIVER_NAME}: log file open error. {}", e);
+            return Err(e.into());
+        }
+    };
+
+    builder.target(Target::Pipe(Box::new(file)));
+    Ok(())
 }
