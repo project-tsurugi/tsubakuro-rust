d0f2d2c0caf901dcb62015af394fafc179ff3f90
Hishida Masato <hishida@nautilus-technologies.com>
2025-08-06 10:39:56 +0900

    fix: embed-resource

--- tsubakuro-rust-odbc/build.rs
+++ tsubakuro-rust-odbc/build.rs
@@ -1,8 +1,16 @@
-extern crate winres;
+extern crate embed_resource;
 
 fn main() {
-    if cfg!(target_os = "windows") {
-        let res = winres::WindowsResource::new();
-        res.compile().unwrap();
-    }
+    let macros = &[
+        format!("REPLACE_VERSION=\"{}\"", env!("CARGO_PKG_VERSION")),
+        format!(
+            "REPLACE_VERSION_RAW={},{},{},0",
+            env!("CARGO_PKG_VERSION_MAJOR"),
+            env!("CARGO_PKG_VERSION_MINOR"),
+            env!("CARGO_PKG_VERSION_PATCH")
+        ),
+    ];
+    embed_resource::compile("resources/versioninfo.rc", macros)
+        .manifest_optional()
+        .unwrap();
 }
