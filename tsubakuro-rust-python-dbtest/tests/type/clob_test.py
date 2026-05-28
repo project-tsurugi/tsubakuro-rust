9e742587f135325b018eefa5c4662fd8562a6940
Hishida Masato <hishida@nautilus-technologies.com>
2026-05-28 09:28:25 +0900

    test(tsubakuro-rust-python): blob relay

---
+++ tsubakuro-rust-python-dbtest/tests/type/clob_test.py
@@ -0,0 +1,243 @@
+import tsurugi_dbapi as tsurugi
+
+
+def drop_and_create_table(connection):
+    with connection.cursor() as cursor:
+        cursor.execute("drop table if exists tsubakuro_rust_python_test")
+        cursor.execute(
+            "create table tsubakuro_rust_python_test (pk int primary key, value clob)"
+        )
+        connection.commit()
+
+
+def test_clob(connection):
+    drop_and_create_table(connection)
+
+    assert connection.lob_transfer_type() == tsurugi.LobTransferType.RELAY
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "insert into tsubakuro_rust_python_test values (0, null), (1, 'abc'), (2, 'def'), (3, '')"
+        )
+        connection.commit()
+
+        metadata = connection.get_table_metadata("tsubakuro_rust_python_test")
+        description = metadata.description
+        assert description == (
+            ("pk", "Int32", None, None, None, None, False),
+            ("value", "Clob", None, None, None, None, True),
+        )
+
+        cursor.execute("select * from tsubakuro_rust_python_test order by pk")
+        rows = cursor.fetchall()
+        assert rows == [
+            (0, None),
+            (1, "abc"),
+            (2, "def"),
+            (3, ""),
+        ]
+        assert type(rows[1][1]) is str
+        description = cursor.description
+        assert description == (
+            ("pk", "Int32", None, None, None, None, None),
+            ("value", "Clob", None, None, None, None, None),
+        )
+        connection.commit()
+
+
+def test_placeholder(connection):
+    drop_and_create_table(connection)
+
+    with connection.cursor() as cursor:
+        parameters = [
+            (1, tsurugi.type_code.Clob("abc")),
+            (2, "def"),
+            (3, ""),
+            (4, None),
+        ]
+        cursor.executemany(
+            "insert into tsubakuro_rust_python_test values (?, ?)", parameters
+        )
+        connection.commit()
+
+        cursor.execute("select * from tsubakuro_rust_python_test order by pk")
+        rows = cursor.fetchall()
+        assert rows == [
+            (1, "abc"),
+            (2, "def"),
+            (3, ""),
+            (4, None),
+        ]
+        connection.commit()
+
+
+def test_placeholder_wrapper(connection):
+    drop_and_create_table(connection)
+
+    with connection.cursor() as cursor:
+        parameters = [
+            (tsurugi.type_code.Int32(0), tsurugi.type_code.Clob(None)),
+            (1, "abc"),
+            (2, "def"),
+            (3, ""),
+        ]
+        cursor.executemany(
+            "insert into tsubakuro_rust_python_test values (?, ?)", parameters
+        )
+        assert cursor.rowcount == 4
+        connection.commit()
+
+        cursor.execute("select * from tsubakuro_rust_python_test order by pk")
+        rows = cursor.fetchall()
+        assert rows == [
+            (0, None),
+            (1, "abc"),
+            (2, "def"),
+            (3, ""),
+        ]
+        connection.commit()
+
+
+def test_named_placeholder(connection):
+    drop_and_create_table(connection)
+
+    with connection.cursor() as cursor:
+        parameters = [
+            {"pk": tsurugi.type_code.Int32(0), "value": tsurugi.type_code.Clob(None)},
+            {
+                "pk": tsurugi.type_code.Int32(1),
+                "value": tsurugi.type_code.Clob("abc"),
+            },
+            {
+                "pk": tsurugi.type_code.Int32(2),
+                "value": tsurugi.type_code.Clob("def"),
+            },
+        ]
+        cursor.executemany(
+            "insert into tsubakuro_rust_python_test values (:pk, :value)", parameters
+        )
+        assert cursor.rowcount == 3
+
+        parameters = [
+            {"pk": 3, "value": "ghi"},
+            {"pk": 4, "value": ""},
+        ]
+        cursor.executemany(
+            "insert into tsubakuro_rust_python_test values (:pk, :value)", parameters
+        )
+        assert cursor.rowcount == 2
+        connection.commit()
+
+        cursor.execute("select * from tsubakuro_rust_python_test order by pk")
+        rows = cursor.fetchall()
+        assert rows == [
+            (0, None),
+            (1, "abc"),
+            (2, "def"),
+            (3, "ghi"),
+            (4, ""),
+        ]
+        connection.commit()
+
+
+def test_prepare_qmark(connection):
+    drop_and_create_table(connection)
+
+    with connection.cursor() as cursor:
+        insert = "insert into tsubakuro_rust_python_test values (?, ?)"
+        cursor.prepare(
+            insert,
+            (tsurugi.type_code.Int32, tsurugi.type_code.Clob),
+        )
+        parameters = [
+            (0, None),
+            (1, "abc"),
+        ]
+        cursor.executemany(insert, parameters)
+        assert cursor.rowcount == 2
+
+        cursor.prepare(
+            insert,
+            (tsurugi.type_code.Int32(), tsurugi.type_code.Clob()),
+        )
+        parameters = [
+            (2, "def"),
+            (3, ""),
+        ]
+        cursor.executemany(insert, parameters)
+        assert cursor.rowcount == 2
+        connection.commit()
+
+        cursor.execute("select * from tsubakuro_rust_python_test order by pk")
+        rows = cursor.fetchall()
+        assert rows == [
+            (0, None),
+            (1, "abc"),
+            (2, "def"),
+            (3, ""),
+        ]
+
+
+def test_prepare_named(connection):
+    drop_and_create_table(connection)
+
+    with connection.cursor() as cursor:
+        insert = "insert into tsubakuro_rust_python_test values (:pk, :value)"
+        cursor.prepare(
+            insert,
+            {"pk": tsurugi.type_code.Int32, "value": tsurugi.type_code.Clob},
+        )
+        parameters = [
+            {"pk": 0, "value": None},
+            {"pk": 1, "value": "abc"},
+        ]
+        cursor.executemany(insert, parameters)
+        assert cursor.rowcount == 2
+
+        cursor.prepare(
+            insert,
+            {"pk": tsurugi.type_code.Int32(), "value": tsurugi.type_code.Clob()},
+        )
+        parameters = [
+            {"pk": 2, "value": "def"},
+            {"pk": 3, "value": ""},
+        ]
+        cursor.executemany(insert, parameters)
+        assert cursor.rowcount == 2
+        connection.commit()
+
+        cursor.execute("select * from tsubakuro_rust_python_test order by pk")
+        rows = cursor.fetchall()
+        assert rows == [
+            (0, None),
+            (1, "abc"),
+            (2, "def"),
+            (3, ""),
+        ]
+
+
+def test_placeholder_upload(connection):
+    drop_and_create_table(connection)
+
+    with connection.cursor() as cursor:
+        parameters = [
+            (0, cursor.upload_clob(None)),
+            (1, cursor.upload_clob("abc")),
+            (2, cursor.upload_clob("def", 1)),
+            (3, cursor.upload_clob("")),
+        ]
+        cursor.executemany(
+            "insert into tsubakuro_rust_python_test values (?, ?)", parameters
+        )
+        assert cursor.rowcount == 4
+        connection.commit()
+
+        cursor.execute("select * from tsubakuro_rust_python_test order by pk")
+        rows = cursor.fetchall()
+        assert rows == [
+            (0, None),
+            (1, "abc"),
+            (2, "def"),
+            (3, ""),
+        ]
+        connection.commit()
